e9ccf6d791b61bd98c73460c5abca9d1164fdbe0
root <root@default.hostname>
2015-03-08 10:32:44 -0400

    地理位置获取

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
 /**
- * wechat php test
- */
+  * wechat php test
+  */
 
 //define your token
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
-if(isset($_GET['echostr'])){
-    $wechatObj->valid(); 
-}
-else{
+
+if (isset($_GET['echostr'])) {
+    $wechatObj->valid();
+}else{
     $wechatObj->responseMsg();
 }
-
 
 class wechatCallbackapiTest
 {
@@ -26,23 +25,38 @@ class wechatCallbackapiTest
         	exit;
         }
     }
-    
-    
 
 
 
 
 
-    public function responseMsg()
+
+
+
+
+
+
+
+
+
+
+    /*public function responseMsg()
     {
-        $postStr = $GLOBALS['HTTP_RAW_POST_DATA'];
-        if(!empty($postStr)){
-            $postObj = simplexml_load_string($postStr,'SimpleXMLElement',LIBXML_NOCDATA);
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr))
+        {
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself 
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
             $RX_TYPE = trim($postObj->msgType);
 
             $result = "";
 
-            switch ($RX_TYPE)
+            switch($RX_TYPE)
             {
                 case "event":
                 $result = $this->receiveEvent($postObj);
@@ -53,72 +67,129 @@ class wechatCallbackapiTest
             }
             echo $result;
         }
-        else{
+        else
+        {
+        	echo "";
+        	exit;
+        }
+    }
+
+     private function receiveEvent($obj)
+    {
+        switch($obj->Event)
+        {
+            case "subscribe":
+                $content =  "欢迎关注课表查询123";
+                $result = $this->transmitText($obj,$content);
+                break;
+            case "LOCATION":
+                $result = $this->position($obj);
+                break;
+        }
+        return $result;
+    }*/
+
+    private function position($obj)
+    {
+        $latitude = $obj->Latitude;
+        $longitude = $obj->Longitude;
+        $content = $latitude."|".$longitude;
+        $result = $this->transmitText($obj,$content);
+        return $result;
+    }
+
+
+
+
+
+
+
+
+ public function responseMsg()
+    {
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        if (!empty($postStr)){
+            $this->logger("R ".$postStr);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $RX_TYPE = trim($postObj->MsgType);
+
+            $result = "";
+            switch ($RX_TYPE)
+            {
+                case "event":
+                    $result = $this->receiveEvent($postObj);
+                    break;
+                case "text":
+                    $result = $this->receiveText($postObj);
+                    break;
+            }
+            $this->logger("T ".$result);
+            echo $result;
+        }else {
             echo "";
             exit;
         }
     }
 
-
-    private function receiveEvent($obj)
+    private function receiveEvent($object)
     {
-        switch($obj->Event)
+        switch ($object->Event)
         {
             case "subscribe":
-            $content =  "欢迎关注课表查询123";
-            break;
+                $content = "欢迎关注方倍工作室 ";
+		$result = $this->transmitText($object, $content);
+                break;
+	    case "LOCATION":
+		$result = $this->position($object);		
         }
-        $result = $this->transmitText($obj,$content);
+        
         return $result;
     }
 
-    private function receiveText($obj)
+    private function receiveText($object)
     {
-        $keyword = trim($obj->Content);
-        $url = "http://apix.sinaapp.com/weather/?appkey=".$object->ToUserName."&city=".urlencode($keyword);
+        $keyword = trim($object->Content);
+        $url = "http://apix.sinaapp.com/weather/?appkey=".$object->ToUserName."&city=".urlencode($keyword); 
         $output = file_get_contents($url);
-        $content = json_decode($output,true);
+        $content = json_decode($output, true);
 
-        $result = $this->transmitNews($obj,$content);
+        $result = $this->transmitNews($object, $content);
         return $result;
     }
 
-    
-    private function transmitText($obj,$content)
+    private function transmitText($object, $content)
     {
-        if(!isset($content) || empty($content))
+        if (!isset($content) || empty($content)){
             return "";
-        $textTpl ="
-<xml>
-<ToUserName><!CDATA[%s]></ToUserName>
-<FromUserName><!CDATA[%s]></FromUserName>
+        }
+        $textTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
 <CreateTime>%s</CreateTime>
-<MsgType><!CDATA[text]></MsgType>
-<Content><!CDATA[%s]></Content>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[%s]]></Content>
 </xml>";
-        $result = sprintf($textTpl,$obj->FromUserName,time(),$content);
+        $result = sprintf($textTpl, $object->FromUserName, $object->ToUserName, time(), $content);
         return $result;
     }
 
-    private function transmitNews($obj,$newsArray)
+    private function transmitNews($object, $newsArray)
     {
-        if(!is_array($newsArray))
+        if(!is_array($newsArray)){
             return "";
-        $itemTpl = "
-  <item>
+        }
+        $itemTpl = "    <item>
         <Title><![CDATA[%s]]></Title>
         <Description><![CDATA[%s]]></Description>
         <PicUrl><![CDATA[%s]]></PicUrl>
         <Url><![CDATA[%s]]></Url>
-  </item>
+    </item>
 ";
         $item_str = "";
-        foreach($newsArray as $item)
-        {
-            $item_str.= sprintf($itemTpl,$item["Title"],$item['Description'],$item['PicUrl'],$item['Url']);
+        foreach ($newsArray as $item){
+            $item_str .= sprintf($itemTpl, $item['Title'], $item['Description'], $item['PicUrl'], $item['Url']);
         }
-        $newsTpl = "
-<xml>
+        $newsTpl = "<xml>
 <ToUserName><![CDATA[%s]]></ToUserName>
 <FromUserName><![CDATA[%s]]></FromUserName>
 <CreateTime>%s</CreateTime>
@@ -128,39 +199,54 @@ class wechatCallbackapiTest
 <Articles>
 $item_str</Articles>
 </xml>";
-        $result = sprintf($newsTpl,$obj->FromUserName,$obj->ToUserName,time(),count($newsArray));
+
+        $result = sprintf($newsTpl, $object->FromUserName, $object->ToUserName, time(), count($newsArray));
         return $result;
+    }
+
+    private function logger($log_content)
+    {
+      
     }
 
 
 
 
-    private function checkSignature()
-    {
+
+
+
+
+
+
+
+
+
+
+
+	private function checkSignature()
+	{
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
         }
+
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
 
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
 
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
-    
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
 
 ?>
